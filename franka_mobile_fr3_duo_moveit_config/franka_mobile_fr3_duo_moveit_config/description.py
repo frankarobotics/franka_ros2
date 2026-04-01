37949e46b0bbe5da49c38b2f5aec461d86183ae6
Marius Winkelmeier <marius.winkelmeier@franka.de>
2026-04-01 13:32:28 +0000

    refactor: apply virtual planar joint by wrapping mobile_fr3_duo_v0_2.xacro to avoid moveit-specific components inside franka_description

--- franka_mobile_fr3_duo_moveit_config/franka_mobile_fr3_duo_moveit_config/description.py
+++ franka_mobile_fr3_duo_moveit_config/franka_mobile_fr3_duo_moveit_config/description.py
@@ -4,19 +4,11 @@ import xacro
 
 def get_robot_descriptions(robot_name, use_fake_hardware, simulate_in_gazebo):
     """Return (robot_description, robot_description_semantic) XML strings."""
-    if simulate_in_gazebo == "true":
-        robot_description_file_path = os.path.join(
-            get_package_share_directory("franka_gazebo_bringup"),
-            "urdf",
-            f"{robot_name}.gazebo.urdf.xacro",
-        )
-    else:
-        robot_description_file_path = os.path.join(
-            get_package_share_directory("franka_description"),
-            "robots",
-            robot_name,
-            f"{robot_name}.urdf.xacro",
-        )
+    robot_description_file_path = os.path.join(
+        get_package_share_directory("franka_mobile_fr3_duo_moveit_config"),
+        "urdf",
+        f"{robot_name}.moveit.urdf.xacro",
+    )
 
     robot_description_semantic_file_path = os.path.join(
         get_package_share_directory("franka_description"),
@@ -28,18 +20,9 @@ def get_robot_descriptions(robot_name, use_fake_hardware, simulate_in_gazebo):
     robot_description = xacro.process_file(
         robot_description_file_path,
         mappings={
-            "robot_ips": "['172.16.16.10', '172.16.16.12', '172.16.16.11']",
-            "robot_prefixes": "['', 'left', 'right']",
-            "robot_types": "['tmrv0_2', 'fr3v2', 'fr3v2']",
-            "hand": "false",
-            "load_gripper": "false",
-            "ee_id": "None",
             "use_fake_hardware": use_fake_hardware,
-            "ros2_control": "true",
+            "gazebo": simulate_in_gazebo,
             "gazebo_effort": simulate_in_gazebo,
-            "fake_sensor_commands": "false",
-            "is_async": "true",
-            "thread_priority": "97",
         },
     ).toxml()
 
